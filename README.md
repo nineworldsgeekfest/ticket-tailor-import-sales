97ff90205b56d611808773b4c8c7f2bdc3d07357
Daniel Whitston <dan@whitston.org.uk>
2016-12-27 01:01:54 +0100

    navigate to shopping cart

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ticket-tailor-import-sales
 A simple import script for Ticket Tailor sales.
 
-Note that libfontconfig is a silent requirement for PhantomJS:
-`sudo apt-get install libfontconfig`
+Uses Mechanize gem for HTTP communication.
+
+## Login details
+The login details are stored in `.config/login.yml` which is ignored by Git and thus not included in this repo. The keys are username, password, eventid, which you should set to your username and password and the number Ticket Tailor uses to represent the event before running.
--- main.rb
+++ main.rb
@@ -1,3 +1,36 @@
 #!/usr/bin/env ruby
 require 'mechanize'
-puts 'hello'
+require 'yaml'
+# There isn't any separation between development and production at this stage
+require 'pry'
+
+# Get login details
+ticket_tailor_login = YAML.load_file('.config/login.yml')
+@username = ticket_tailor_login['username']
+@password = ticket_tailor_login['password'].to_s
+@event = ticket_tailor_login['event'].to_s
+
+# Initialise the Mechanize agent
+agent = Mechanize.new
+
+# Load, fill in and submit the login page
+page = agent.get 'https://www.tickettailor.com/login'
+form = page.form
+form['username'] = @username
+form['password'] = @password
+page = form.submit
+
+# Navigate to the tickets page via a route that validates CSRF
+page = page.links.find {|link|link.uri.to_s.include? 'buytickets.at'}.click
+page = page.links.find {|link| link.uri.to_s.include? '/checkout/view-event/id/' + @event }.click
+
+# Open a CSV file with upload data
+
+# Enter ticket quantities into shopping cart
+form = page.form
+pp form
+binding.pry
+
+# Enter purchaser details into checkout form
+
+# Enter transaction ID into conrirmation form and confirm allocation
